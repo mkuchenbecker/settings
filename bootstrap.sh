6759c7347cbfeab1fdc71fc7e2d7523a6634171f
Mike Kuchenbecker <34144575+mkuchenbecker@users.noreply.github.com>
2019-11-12 21:52:57 -0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,3 +3,5 @@ git clone https://github.com/mkuchenbecker/settings.git
 cp settings/vim.vim .vimrc 
 sudo apt-get -y install vim
 alias vi=vim
+
+git clone https://github.com/mkuchenbecker/brewery3.git
